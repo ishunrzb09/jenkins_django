f93af38a00ff5b85b86bc379e39676d8e41f3ae7
gaurav.singh9 <gaurav.singh@petco.com>
2023-01-05 18:53:31 +0530

    black test

--- chakki/chakki/urls.py
+++ chakki/chakki/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("udhari/", include("udhari.urls")),
+    path("admin/", admin.site.urls),path("udhari/", include("udhari.urls")),
     path("purji/", include("purji.urls")),
 ]
