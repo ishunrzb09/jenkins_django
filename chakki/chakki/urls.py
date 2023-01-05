b858edf53ca6498e5f6508361ab4d6b2aa5b291a
gaurav.singh9 <gaurav.singh@petco.com>
2023-01-05 15:46:53 +0530

    added

--- chakki/chakki/urls.py
+++ chakki/chakki/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("udhari", include("udhari.urls"))
+    path("udhari/", include("udhari.urls"))
 ]
