c462f42e81c1687cc34512d69d906ac215b7f2c0
gaurav.singh9 <gaurav.singh@petco.com>
2023-01-05 18:12:14 +0530

    ff

--- chakki/chakki/urls.py
+++ chakki/chakki/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    
-    path("admin/", admin.site.urls),
-    path("udhari/", include("udhari.urls")),
-    path("purji/", include("purji.urls")),
+urlpatterns = [  
+    path("admin/", admin.site.urls),path("udhari/", include("udhari.urls")),path("purji/", include("purji.urls")),
 ]
