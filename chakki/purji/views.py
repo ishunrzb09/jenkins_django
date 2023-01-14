6ee66bcd2cd6197775b4502096be92bc26d4d968
gaurav.singh9 <gaurav.singh@petco.com>
2023-01-14 11:09:18 +0530

    fixed all

--- chakki/purji/views.py
+++ chakki/purji/views.py
@@ -1,10 +1,9 @@
 import email
 
-from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    xyz = [1, 2, 3, 4, 5, 6, 7, 8]
     return HttpResponse("purji url")
